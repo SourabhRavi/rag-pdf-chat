b979a12a49195c1d21ce299cd0cef55792321d9e
SourabhRavi <sourabh.k.ravi@gmail.com>
2026-08-17 23:48:23 +0530

    update chat rate limit

--- server/middleware/rate-limit.middleware.js
+++ server/middleware/rate-limit.middleware.js
@@ -2,7 +2,7 @@ const { rateLimit } = require("express-rate-limit");
 
 const chatRateLimiter = rateLimit({
   windowMs: 60 * 1000,
-  limit: 2,
+  limit: 10,
   message: {
     success: false,
     message: "Too many chat requests. Please try again later.",
