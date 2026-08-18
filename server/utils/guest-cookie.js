23d5b0a4bf7b4f3ecec657c7fbf66c978f065c5c
SourabhRavi <sourabh.k.ravi@gmail.com>
2026-08-18 21:49:48 +0530

    refactor guest cookie settings to use 'lax' for sameSite attribute

--- server/utils/guest-cookie.js
+++ server/utils/guest-cookie.js
@@ -2,7 +2,7 @@ const setGuestCookie = (res, guestId) => {
   res.cookie("guestId", guestId, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    sameSite: "lax",
     maxAge: 1000 * 60 * 60 * 24 * 30,
   });
 };
